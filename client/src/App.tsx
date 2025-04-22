faa8fd49f0e23128a09194fc8b149a4c1d498328
TRISTAN ZUBIARRAIN <TRISTANZUBIARRAIN15@gmail.com>
2025-04-22 14:14:33 +0200

    work in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import MenuPage from "./pages/MenuPage/MenuPage";
 
 function App() {
   return (
     <>
       <Header />
+      <MenuPage />
     </>
   );
 }
---
+++ client/src/pages/MenuPage/MenuPage.tsx
@@ -0,0 +1,11 @@
+import "./MenuPage.css";
+
+function MenuPage() {
+  return (
+    <section>
+      <h1>LE MENU</h1>
+    </section>
+  );
+}
+
+export default MenuPage;
