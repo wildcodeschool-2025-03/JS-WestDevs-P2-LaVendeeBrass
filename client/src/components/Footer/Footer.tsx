7bff81975d127ee32015efe7d15934539e4ff0a2
Eula-Coly <eulacoly@gmail.com>
2025-04-22 16:45:47 +0200

    working progress

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,47 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <h3>Contact:</h3>
+          <p>Téléphone: 02-44-56-97-83</p>
+          <p>E-mail: contact@vendee-brass.fr</p>
+          <p>Adresse : 52 Av. de la plage 85800 Saint-Gilles-Croix-de-vie</p>
+        </li>
+
+        <li className="reseaux">
+          <h3>Réseaux</h3>
+          <section>
+            <a href=" ">
+              <img
+                src="logo-facebook.svg"
+                alt="Lien vers notre page Facebook"
+              />
+            </a>
+            <a href=" ">
+              <img
+                src="logo-instagram.svg"
+                alt="Lien vers notre page Instagram"
+              />
+            </a>
+
+            <a href=" ">
+              <img src="logo-X.png" alt="Lien vers notre page Twitter" />
+            </a>
+          </section>
+        </li>
+
+        <li>
+          <h3>Le Site</h3>
+          <p>Site développé par WestDevs</p>
+          <a href=" ">Mentions légales </a>
+          <a href=" ">Données personnelles</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
